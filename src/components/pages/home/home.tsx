a08f5be99c5d3509600bad57f2baf97d43d104c7
IcyDefiance <icydefiance@gmail.com>
2019-10-27 16:04:09 -0400

    Add rms link

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,4 +1,10 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 
-export const Home: React.FC = () => <Link to="/profile/Gargron@mastodon.social">Gargron@mastodon.social</Link>;
+export const Home: React.FC = () => (
+	<>
+		<Link to="/profile/Gargron@mastodon.social">Gargron@mastodon.social</Link>
+		<br />
+		<Link to="/profile/rms@gnusocial.no">rms@gnusocial.no</Link>
+	</>
+);
